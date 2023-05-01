8dd2620cff7b8ef3cd708a966831371260073cb5
MrBlubDev <mrblub2010@icloud.com>
2023-05-01 20:23:49 +0100

    navbar

--- my-project/src/pages/_app.tsx
+++ my-project/src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <div className="navbar">
+        <Image
+          src="/logo.png"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
