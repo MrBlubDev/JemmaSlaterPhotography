ebd14316a53aba4a17cf9ed53b086608fbffa660
MrBlubDev <mrblub2010@icloud.com>
2023-05-02 13:42:19 +0100

    commit

--- my-project/src/pages/_app.tsx
+++ my-project/src/pages/_app.tsx
@@ -6,12 +6,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <div className="navbar">
+        <div className="button-container">
+          <button className="navbar-button">HOME</button>
+          <button className="navbar-button">THE STUDIO</button>
+          <button className="navbar-button">PHOTOSHOOTS</button>
+          <button className="navbar-button">PRODUCTS</button>
+        </div>
         <Image
           src="/logo.png"
           alt="Picture of the author"
-          width={500}
-          height={500}
+          className="logo"
+          width={520}
+          height={520}
         />
+        <div className="button-container">
+          <button className="navbar-button">FAQ&apos;S</button>
+          <button className="navbar-button">REVIEWS</button>
+          <button className="navbar-button">BLOG</button>
+          <button className="navbar-button">CONTACT</button>
+        </div>
       </div>
       <Component {...pageProps} />
     </div>
